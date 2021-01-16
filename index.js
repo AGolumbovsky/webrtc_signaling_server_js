3796226a51cc20ed2a8a3dd4be4dce78ced25300
Andriy <agolumbovsky@gmail.com>
2021-01-15 19:49:29 -0500

    require(sock.io)(http) to fix not a fn error

--- index.js
+++ index.js
@@ -3,12 +3,13 @@
 var os = require('os');
 var nodeStatic = require('node-static');
 var http = require('http');
-var socketIO = require('socket.io');
+var socketIO = require('socket.io')(http);
+// var io = require('socket.io')(http);
 
 var fileServer = new(nodeStatic.Server)();
 var app = http.createServer(function(req, res) {
   fileServer.serve(req, res);
-}).listen(8080);
+}).listen(8080, () => {console.log("test message server running")});
 
 var io = socketIO.listen(app);
 io.sockets.on('connection', function(socket) {
